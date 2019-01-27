1a8db7839c535d85c73714ac99fde67975a0ec05
SR <vijayadurga12v12@gmail.com>
2019-01-27 07:29:19 -0800

    hi Signed-off-by: SR <vijayadurga12v12@gmail.com>

---
+++ src/main/java/techstyle/techweb/App.java
@@ -0,0 +1,13 @@
+package techstyle.techweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/techstyle/techweb/GmailCount.java
@@ -0,0 +1,48 @@
+package techstyle.techweb;
+
+
+		
+
+		import java.text.SimpleDateFormat;
+		import java.util.Date;
+		import java.util.List;
+
+		import org.openqa.selenium.By;
+		import org.openqa.selenium.WebDriver;
+		import org.openqa.selenium.WebElement;
+		import org.openqa.selenium.chrome.ChromeDriver;
+		import org.openqa.selenium.support.ui.ExpectedConditions;
+		import org.openqa.selenium.support.ui.WebDriverWait;
+
+		public class GmailCount {
+
+			public static void main(String[] args)
+			{
+				// Launch site
+				System.setProperty("webdriver.chrome.driver", "C:\\seljava\\chromedriver.exe");
+				WebDriver d=new ChromeDriver();
+				d.get("https://gmail.com");
+				d.findElement(By.name("identifier")).sendKeys("shylajaammasani@gmail.com");
+				d.findElement(By.xpath("//*[text()='Next']")).click();
+				WebDriverWait wait=new WebDriverWait(d,100);
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+				
+				d.findElement(By.name("password")).sendKeys("Sudhesh1027");
+				d.findElement(By.xpath("//*[text()='Next']")).click();
+				
+				//d.findElement(By.xpath("(//table)[4]/tbody/tr[8]/td[2]")).click();
+				//d.findElement(By.xpath("//div[contains(@class, 'G-tF')]/div[2]/div[3]/div")).click();
+				
+				
+				
+				}
+				
+				
+
+			}
+
+		}
+
+	}
+
+}
